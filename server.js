7d5a4e38e4e77980b223a0f177426b9cff6ca24b
rdwanmuhamad <mrdwn624@gmail.com>
2023-05-19 12:14:13 +0800

    Request Handling Exercise

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
-const http = require('http');
+const http = require("http");
 
 const requestListener = (request, response) => {
-    response.setHeader('Content-Type', 'text/html');
+  response.setHeader("Content-Type", "text/html");
 
-    response.statusCode = 200;
-    response.end('<h1>Halo HTTP Server!</h1>')
+  response.statusCode = 200;
+
+  const { method } = request;
+
+  if (method === "GET") {
+    response.end("<h1>Hello!</h1>");
+  }
+
+  if (method === "POST") {
+    response.end("<h1>Hai!</h1>");
+  }
+
+  if (method === "PUT") {
+    response.end("<h1>Bounjur!</h1>");
+  }
+
+  if (method === "DELETE") {
+    response.end("<h1>Salam!</h1>");
+  }
 };
 
 const server = http.createServer(requestListener);
 
 const port = 5000;
-const host = 'localhost';
+const host = "localhost";
 
 server.listen(port, host, () => {
-    console.log(`Server berjalan pada http://${host}:${port}`)
+  console.log(`Server berjalan pada http://${host}:${port}`);
 });
